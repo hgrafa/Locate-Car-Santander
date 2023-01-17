1bb244bbbbf9a5533d8478c17a55b760e8a82dc1
Chaimsohn <danilo_chaimsohn@hotmail.com>
2023-01-17 20:13:11 -0300

    Teste git danilo

--- src/main/java/com/santander/locatecar/Main.java
+++ src/main/java/com/santander/locatecar/Main.java
@@ -3,5 +3,6 @@ package com.santander.locatecar;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Teste Git");
     }
 }

307d097de728713151fd5c96aa15812e348fff11
vitor-pirovani-paixao <vitorpirovani@gmail.com>
2023-01-17 20:30:41 -0300

    teste branch

--- src/main/java/com/santander/locatecar/Main.java
+++ src/main/java/com/santander/locatecar/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Teste Git");
+        System.out.println("Teste Git2");
     }
 }
